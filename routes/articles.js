dd845278948876a7d0e3f762e3c518c4094c97a7
Binay <binaydas1123@gmail.com>
2024-09-09 10:07:03 +0530

    More routes added

--- routes/articles.js
+++ routes/articles.js
@@ -17,16 +17,20 @@ router.post('/', async (req, res) => {
 
     try {
         await article.save();
-        // res.redirect(`/articles/${article.id}`);
-        res.redirect(`/articles`);
+        res.redirect(`/articles/${article.id}`);
+        // res.redirect(`/articles`);
     } catch (e) {
         console.log(e);
         res.render(`articles/new`, { article: article });
     }
 })
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async(req, res) => {
+    const article = await Article.findById(req.params.id);
+    if (article == null) {
+        req.redirect('/');
+    }
+    res.render('articles/show', {article: article});
 })
 
 module.exports = router;
